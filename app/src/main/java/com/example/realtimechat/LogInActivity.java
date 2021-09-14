1700d3b14a37b18a7e3c72a9ebf4c97e70b2a315
SogYa <Ghost.Spartan36@gmail.com>
2021-09-14 23:22:18 +0300

    feat: add sign in link

---
+++ app/src/main/java/com/example/realtimechat/LogInActivity.java
@@ -0,0 +1,23 @@
+package com.example.realtimechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+    public void onClickGoToRegistration(View view) {
+        startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+    }
+
+    public void onClickLogIn(View view) {
+    }
+}
