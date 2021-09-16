ff555505a8011cb440609850a97a7b3c12bdc920
SogYa <Ghost.Spartan36@gmail.com>
2021-09-16 11:05:19 +0300

    feat: add a check for the correctness of the data entered feat: add checking for the presence of email in the DB fix: crash if if the user has not entered the email or password fix: constant launch of a successful event fix: toast wasn't shown

--- app/src/main/java/com/example/realtimechat/RegistrationActivity.java
+++ app/src/main/java/com/example/realtimechat/RegistrationActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,7 +19,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class RegistrationActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private String mCustomToken;
-    private String TAG = "AuthActivity";
+    private final String TAG = "AuthActivity";
     private String emailAddress,password;
     private EditText emailAddressEditText,passwordEditText;
     @Override
@@ -46,25 +47,25 @@ public class RegistrationActivity extends AppCompatActivity {
 
         emailAddress = emailAddressEditText.getText().toString();
         password = passwordEditText.getText().toString();
-        if(emailAddress.isEmpty()){
-            Toast toast = Toast.makeText(RegistrationActivity.this,"Пожалуйста заполните поля",Toast.LENGTH_SHORT);
-        }
-        else if (password.isEmpty()){
-            Toast toast = Toast.makeText(RegistrationActivity.this,"Пожалуйста заполните поля",Toast.LENGTH_SHORT);
-        }
-        else {
+
+        if(!emailAddress.isEmpty() & !password.isEmpty()) {
+
             mAuth.createUserWithEmailAndPassword(emailAddress, password)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Добро пожаловать!", Toast.LENGTH_SHORT).show();
                                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
                                 finish();
                             } else {
-                                Toast.makeText(getApplicationContext(), "E-mail or password is wrong", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "Неккоректные данные / пользователь уже создан!", Toast.LENGTH_SHORT).show();
                             }
                         }
-                    });
+            });
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Пожалуйста заполните поля",Toast.LENGTH_SHORT).show();
         }
     }
 }
