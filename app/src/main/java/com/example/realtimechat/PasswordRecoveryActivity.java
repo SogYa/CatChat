3c38d4b4caf40d9006d43c8cfbd7ac52b1676087
SogYa <Ghost.Spartan36@gmail.com>
2021-09-16 11:06:10 +0300

    feat: added password recovery screen

---
+++ app/src/main/java/com/example/realtimechat/PasswordRecoveryActivity.java
@@ -0,0 +1,14 @@
+package com.example.realtimechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordRecoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_recovery);
+    }
+}
