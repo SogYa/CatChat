ee73dd4f8d6474d807861dbc6ff40fc3954360d9
SogYa <Ghost.Spartan36@gmail.com>
2021-09-18 22:53:35 +0300

    feat: add start activity include app logo and current version number

---
+++ app/src/main/java/com/example/realtimechat/StartActivity.java
@@ -0,0 +1,24 @@
+package com.example.realtimechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
