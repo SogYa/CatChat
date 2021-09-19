5978a5aa0a603144861b2f1185230dcbcc8639e2
SogYa <Ghost.Spartan36@gmail.com>
2021-09-19 15:45:40 +0300

    fix: wrong layout name

--- app/src/main/java/com/example/realtimechat/LogInActivity.java
+++ app/src/main/java/com/example/realtimechat/LogInActivity.java
@@ -11,7 +11,7 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_sign_in);
     }
 
     public void onClickGoToRegistration(View view) {
