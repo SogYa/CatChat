3fb6ff88c0b865b08f9f73372b96de64f4b805ef
Timofey Sorokin <Ghost.Spartan36@outlook.com>
2021-09-23 21:05:37 +0300

    feat: main chat screen ui

--- app/src/main/java/com/example/realtimechat/MainActivity.java
+++ app/src/main/java/com/example/realtimechat/MainActivity.java
@@ -11,46 +11,16 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String APP_PREFERENCES_CHECK = "LogStatus";
-    SharedPreferences mSettings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSettings = getSharedPreferences(APP_PREFERENCES_CHECK,MODE_PRIVATE);
-    }
-
-    public void onClickLogOut(View view) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (!isFinishing()) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Выход из аккаунта")
-                            .setMessage("Вы уверены, что хотите выйти из аккаунта?")
-                            .setPositiveButton("Да", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    startActivity(new Intent(MainActivity.this, LogInActivity.class));
-                                    SharedPreferences.Editor editor = mSettings.edit();
-                                    editor.putBoolean(APP_PREFERENCES_CHECK, false);
-                                    editor.apply();
-                                    finish();
-                                }
-                            })
-                            .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.cancel();
-                                }
-                            })
-                            .setIcon(R.drawable.ic_baseline_warning)
-                            .show();
-                }
-            }
-        });
 
     }
 
+    public void onClickProfile(View view) {
+        startActivity(new Intent(MainActivity.this,UserProfileActivity.class));
+    }
 }
