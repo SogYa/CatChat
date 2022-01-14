e74d148535fdc006a11800a9729c3d7e105c9f00
SogYa <Ghost.Spartan36@gmail.com>
2022-01-14 20:09:14 +0300

    fix: fixed incorrect status display

--- app/src/main/java/com/example/realtimechat/instruments/AppStatements.java
+++ app/src/main/java/com/example/realtimechat/instruments/AppStatements.java
@@ -1,17 +1,25 @@
 package com.example.realtimechat.instruments;
 
+import com.example.realtimechat.datalayer.SPControl;
+
 public enum AppStatements {
     ONLINE("В сети"),
     OFFLINE("Не в сети");
+    private final String title;
 
-    AppStatements(String appState) {
+    public String getTitle() {
+        return title;
+    }
+
+    AppStatements(String title) {
+        this.title = title;
     }
 
     public static void sendOnline(){
-        Constants.PATH_TO_USER_STATUS.setValue(ONLINE.toString());
+        Constants.PATH_TO_USER.child(SPControl.getInstance().getStringPrefs(Constants.APP_PREFS_USER_ID)).child("status").setValue(ONLINE.getTitle());
     }
 
     public static void sendOffline() {
-        Constants.PATH_TO_USER_STATUS.setValue(OFFLINE.toString());
+        Constants.PATH_TO_USER.child(SPControl.getInstance().getStringPrefs(Constants.APP_PREFS_USER_ID)).child("status").setValue(OFFLINE.getTitle());
     }
 }
