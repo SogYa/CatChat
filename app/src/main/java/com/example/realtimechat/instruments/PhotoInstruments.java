d32fc020c769fce4c0b0a39a904ea39849d4fb84
SogYa <Ghost.Spartan36@gmail.com>
2022-01-14 19:21:42 +0300

    chore: added new methods

--- app/src/main/java/com/example/realtimechat/instruments/PhotoInstruments.java
+++ app/src/main/java/com/example/realtimechat/instruments/PhotoInstruments.java
@@ -3,7 +3,6 @@ package com.example.realtimechat.instruments;
 import android.app.Activity;
 import android.net.Uri;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 import com.example.realtimechat.R;
 import com.example.realtimechat.app.App;
@@ -12,17 +11,21 @@ import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class PhotoInstruments {
+
     private final AuthRepo authRepo = new AuthRepo();
-    public void downloadAndSetImage(String uid, ImageView imageView) {
+
+    public Void downloadAndSetImage(String uid, ImageView imageView) {
         authRepo.readUserFromDataBase(uid, value -> Glide.with(App.getAppContext())
                 .load(Uri.parse(value.getPhotoUrl()))
                 .placeholder(R.mipmap.ic_deafault_avatar)
                 .into(imageView));
+        return null;
     }
+
     public void setImage(Activity activity) {
         CropImage.activity()
                 .setAspectRatio(1, 1)
-                .setRequestedSize(150, 150)
+                .setRequestedSize(600, 600)
                 .setCropShape(CropImageView.CropShape.OVAL)
                 .start(activity);
     }
