70d6cb320277cf08a29a7997ab46569e5790627c
sogya <ghost.spartan36@g,ail.com>
2022-03-29 19:49:52 +0300

    fix: fixed bug with back button

--- app/src/main/java/com/example/realtimechat/MainActivity.kt
+++ app/src/main/java/com/example/realtimechat/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.realtimechat
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
@@ -31,7 +29,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         super.onBackPressed()
-        supportFragmentManager.popBackStack()
+        for (i in 0 until supportFragmentManager.backStackEntryCount) {
+            supportFragmentManager.popBackStack()
+        }
     }
 
 }
