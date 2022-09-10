c7f691bbfed363a001b0d6c10cc302f9aeae3e2d
SogYa <Ghost.Spartan36@gmail.com>
2022-09-10 14:16:02 +0300

    added new recyclerview adapter

---
+++ app/src/main/java/com/example/realtimechat/screens/screenMenu/MenuAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.realtimechat.screens.screenMenu
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MenuAdapter :
+    RecyclerView.Adapter<com.example.realtimechat.screens.screenMenu.MenuAdapter.ViewHolder>() {
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+// TODO: ("Сделать список иконок")
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
