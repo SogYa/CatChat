2cb86f222bf43aaec02fc421046bffb4de29f64e
SogYa <Ghost.Spartan36@gmail.com>
2022-09-10 14:00:56 +0300

    added feed fragment

---
+++ app/src/main/java/com/example/realtimechat/screens/screenFeed/FeedFragment.kt
@@ -0,0 +1,5 @@
+package com.example.realtimechat.screens.screenFeed
+
+import androidx.fragment.app.Fragment
+
+class FeedFragment : Fragment()
---
+++ app/src/main/java/com/example/realtimechat/screens/screenFeed/FeedVM.kt
@@ -0,0 +1,5 @@
+package com.example.realtimechat.screens.screenFeed
+
+import androidx.lifecycle.ViewModel
+
+class FeedVM : ViewModel()
