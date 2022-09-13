ae0f8d00511a6c4b46b2b60c50b57eb1937acb2d
SogYa <Ghost.Spartan36@gmail.com>
2022-09-13 15:55:21 +0300

    recyclerview realization

--- app/src/main/java/com/example/realtimechat/screens/screenMenu/MenuAdapter.kt
+++ app/src/main/java/com/example/realtimechat/screens/screenMenu/MenuAdapter.kt
@@ -1,27 +1,55 @@
 package com.example.realtimechat.screens.screenMenu
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.realtimechat.R
+import com.example.realtimechat.data.model.MenuItem
+import de.hdodenhof.circleimageview.CircleImageView
 
-class MenuAdapter :
+class MenuAdapter(onClickListener: OnMenuItemClickListener) :
     RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
-
+    private val menuItems: ArrayList<MenuItem> = ArrayList()
+    private val onMenuItemClickListener: OnMenuItemClickListener = onClickListener
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-// TODO: ("Сделать список иконок")
+        val labelText: TextView = itemView.findViewById(R.id.menuLabelText)
+        val menuIcon: CircleImageView = itemView.findViewById(R.id.menuIconImageIVew)
+        val menuMainText: TextView = itemView.findViewById(R.id.menuMainText)
 
+    }
+
+    interface OnMenuItemClickListener {
+        fun onClick(menuItem: MenuItem, position: Int)
+    }
+
+    fun updateMenuList(menuItemList: ArrayList<MenuItem>) {
+        this.menuItems.clear()
+        notifyItemChanged(1)
+        this.menuItems.addAll(menuItemList)
+        notifyItemRangeChanged(0, menuItems.size)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view: View =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.menu_list_item, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val menuItems = menuItems[position]
+        holder.menuIcon.setImageResource(menuItems.iconId)
+        holder.menuMainText.text = menuItems.text
+        holder.labelText.text = menuItems.title
+        holder.itemView.setOnClickListener {
+            onMenuItemClickListener.onClick(menuItems, position)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return menuItems.size
     }
 }
--- app/src/main/java/com/example/realtimechat/screens/screenMenu/MenuFragment.kt
+++ app/src/main/java/com/example/realtimechat/screens/screenMenu/MenuFragment.kt
@@ -5,18 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.realtimechat.R
+import com.example.realtimechat.data.model.MenuItem
 import com.example.realtimechat.databinding.FragmentMenuBinding
 
-class MenuFragment : Fragment(R.layout.fragment_menu) {
+class MenuFragment : Fragment(R.layout.fragment_menu), MenuAdapter.OnMenuItemClickListener {
+    private lateinit var menuAdapter: MenuAdapter
     private lateinit var binding: FragmentMenuBinding
+    private lateinit var vm: MenuVM
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMenuBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vm = ViewModelProvider(this).get(MenuVM::class.java)
+        menuAdapter = MenuAdapter(this)
+        val linearLayoutManager = LinearLayoutManager(context)
+        binding.menuRecyclerview.layoutManager = linearLayoutManager
+        binding.menuRecyclerview.adapter = menuAdapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        vm.getMenuLiveData().observe(this.viewLifecycleOwner) {
+            menuAdapter.updateMenuList(it)
+        }
+    }
+
+    override fun onClick(menuItem: MenuItem, position: Int) {
+        when (menuItem.title) {
+            "Общий чат" -> findNavController().navigate(R.id.action_menuFragment_to_chatFragment2)
+        }
     }
 }
