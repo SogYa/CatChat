3347183e163f8071613778e749de575c8cea7690
SogYa <Ghost.Spartan36@gmail.com>
2022-09-13 15:53:51 +0300

    added new menu objects to list

--- app/src/main/java/com/example/realtimechat/screens/screenMenu/MenuVM.kt
+++ app/src/main/java/com/example/realtimechat/screens/screenMenu/MenuVM.kt
@@ -1,8 +1,18 @@
 package com.example.realtimechat.screens.screenMenu
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.realtimechat.data.model.MenuItem
 
-class MenuVM: ViewModel() {
+class MenuVM : ViewModel() {
 
-    //Todo
+    private val menuLiveData: MutableLiveData<ArrayList<MenuItem>> = MutableLiveData()
+
+    init {
+        menuLiveData.postValue(MenuItem.menuItemsList)
+    }
+
+    fun getMenuLiveData(): MutableLiveData<ArrayList<MenuItem>> {
+        return menuLiveData
+    }
 }
