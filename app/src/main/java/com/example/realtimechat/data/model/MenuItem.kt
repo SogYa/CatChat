eb3ab907e485cd0d4b136bc55db22f7cf9f5bc4c
SogYa <Ghost.Spartan36@gmail.com>
2022-09-13 15:54:41 +0300

    changed list items

--- app/src/main/java/com/example/realtimechat/data/model/MenuItem.kt
+++ app/src/main/java/com/example/realtimechat/data/model/MenuItem.kt
@@ -1,5 +1,35 @@
 package com.example.realtimechat.data.model
 
-import android.graphics.drawable.Drawable
+import com.example.realtimechat.R
 
-data class MenuItem(val icon: Drawable, val title: String, val text: String)
+data class MenuItem(val categoryId: Int, val iconId: Int, val title: String, val text: String) {
+    companion object {
+        val menuItemsList = arrayListOf(
+            MenuItem(
+                1,
+                R.drawable.ic_baseline_chat,
+                "Питомец",
+                "Все данные вашего питомца в одном месте"
+            ),
+            MenuItem(
+                2,
+                R.drawable.ic_baseline_chat,
+                "Уход",
+                "Инструментарий для ухода за вашим питомцем"
+            ),
+
+            MenuItem(
+                3,
+                R.drawable.ic_baseline_chat,
+                "Общий чат",
+                "Чат для общения всех пользователей нашего приложения"
+            ),
+            MenuItem(
+                4,
+                R.drawable.ic_baseline_chat,
+                "Список пользователей",
+                "Список всех участников"
+            )
+        )
+    }
+}
